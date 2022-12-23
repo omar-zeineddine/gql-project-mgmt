6632c6e5837650f13c05d1c3338829334840ec87
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-23 23:08:32 +0200

    fix - add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const colors = require("colors");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
+app.use(cors());
 
 app.use(
   "/graphql",
