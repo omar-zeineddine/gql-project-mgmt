3047cc41e545af063f878471c3b4420bc3680f8f
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-24 21:19:18 +0200

    feat - delete client mutation by updating cache

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,36 @@
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutation";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // one method of refetching
+    // refetchQueries: [{ query: GET_CLIENTS }],
+
+    // method two: updating cache
+    // get query from cache
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({
+        query: GET_CLIENTS,
+      });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-sm btn-danger">
+        <button className="btn btn-sm btn-danger" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
