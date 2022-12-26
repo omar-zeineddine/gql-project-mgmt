6fb4099fda02447a349c6c5f7c3fc6f60bc8003f
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-26 15:48:55 +0200

    feat - add project modal

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { FaList } from "react-icons/fa";
+import { useMutation, useQuery } from "@apollo/client";
+
+function AddProjectModal() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, name, phone);
+    if (name === "" || description === "" || status === "") {
+      return alert("all must be filled");
+    }
+
+    setName("");
+    setDescription("");
+    setStatus("new");
+    setClientId("");
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#addProjectModal"
+      >
+        <div className="d-flex align-items-center">
+          <FaList className="icons" />
+          <div className="mx-2">New Project</div>
+        </div>
+      </button>
+
+      <div
+        className="modal fade"
+        id="addProjectModal"
+        aria-labelledby="addProjectModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="addProjectModalLabel">
+                new project
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="" className="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="form-control"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="form-label">
+                    Description
+                  </label>
+                  <textarea
+                    id="description"
+                    className="form-control"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  ></textarea>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="form-label">
+                    Status
+                  </label>
+                  <select
+                    className="form-select"
+                    id="status"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                  >
+                    <option value="new">Not Started</option>
+                    <option value="progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                  </select>
+                </div>
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  data-bs-dismiss="modal" // closes modal on submit
+                >
+                  submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AddProjectModal;
