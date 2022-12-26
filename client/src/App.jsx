9ae9d614c470e0881155373712e172e642beb376
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-26 10:49:42 +0200

    refactor - add home page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import Clients from "./components/Clients";
-import Projects from "./components/Projects";
 import Header from "./components/Header";
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -32,12 +31,14 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
